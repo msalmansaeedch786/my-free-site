5913e3c4e03f8f0f245f14c8bf1f25cb69dd9de1
Muhammad Salman <msalmansaeedch786@gmail.com>
2026-06-07 02:20:49 +0200

    style: format with black==23.11.0

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ def set_security_headers(response):
     response.headers["X-Content-Type-Options"] = "nosniff"
     response.headers["X-Frame-Options"] = "DENY"
     response.headers["Referrer-Policy"] = "strict-origin-when-cross-origin"
-    response.headers["Strict-Transport-Security"] = (
-        "max-age=31536000; includeSubDomains"
-    )
+    response.headers[
+        "Strict-Transport-Security"
+    ] = "max-age=31536000; includeSubDomains"
     return response
 
 
